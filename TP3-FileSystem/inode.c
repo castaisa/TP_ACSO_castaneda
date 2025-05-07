f6340d4736c55a497f87153c0d167cdd274699e1
castaisa <icastaneda@udesa.edu.ar>
2025-05-07 18:32:09 -0300

    inode_iget

--- TP3-FileSystem/inode.c
+++ TP3-FileSystem/inode.c
@@ -10,7 +10,24 @@
  */
 int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
     //Implement Code Here
-    return 0; 
+    if (inumber < 1) {
+        return -1;  // número de inodo inválido
+    }
+
+    int inodes_per_block = DISKIMG_SECTOR_SIZE / sizeof(struct inode);
+    int inode_block_offset = (inumber - 1) / inodes_per_block;
+    int inode_index_in_block = (inumber - 1) % inodes_per_block;
+
+    int sector = INODE_START_SECTOR + inode_block_offset;
+
+    struct inode buffer[DISKIMG_SECTOR_SIZE / sizeof(struct inode)];
+
+    if (diskimg_readsector(fs->dfd, buffer, sector) < 0) {
+        return -1;  // error al leer el sector
+    }
+
+    *inp = buffer[inode_index_in_block];
+    return 0;  // éxito
 }
 
 /**
