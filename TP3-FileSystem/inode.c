a27177be2f5244a1f89810dea2a59153f4699541
castaisa <icastaneda@udesa.edu.ar>
2025-05-07 18:48:55 -0300

    indexlookup

--- TP3-FileSystem/inode.c
+++ TP3-FileSystem/inode.c
@@ -34,8 +34,34 @@ int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
  */
 int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp,
     int blockNum) {  
-        //Implement code here
-    return 0;
+        if (blockNum < 0) {
+            return -1;
+        }
+    
+        // Casos directos: i_addr[0] a i_addr[5]
+        if (blockNum < 6) {
+            return inp->i_addr[blockNum];
+        }
+    
+        // Caso indirecto simple
+        if (blockNum < 6 + 128) {
+            int indirect_block = inp->i_addr[6];
+            if (indirect_block == 0) {
+                return -1; // No está asignado
+            }
+    
+            uint16_t block_data[256];  // 512 bytes / 2 bytes = 256 punteros
+    
+            if (diskimg_readsector(fs->dfd, indirect_block, block_data) < 0) {
+                return -1;
+            }
+    
+            int indirect_index = blockNum - 6;
+            return block_data[indirect_index];
+        }
+    
+        // No soportamos dobles indirectos
+        return -1;
 }
 
 int inode_getsize(struct inode *inp) {
