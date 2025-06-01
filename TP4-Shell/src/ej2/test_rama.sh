51e6134b3bb8b4d07b62e06de6d1352d30cc18fe
castaisa <icastaneda@udesa.edu.ar>
2025-06-01 19:30:44 -0300

    termino de testear

--- TP4-Shell/src/ej2/test_rama.sh
+++ TP4-Shell/src/ej2/test_rama.sh
@@ -149,7 +149,7 @@ run_test "echo $(seq -s ' ' 1 63)" "Límite exacto de argumentos"
 run_test "echo $(seq -s ' ' 1 64)" "Exceso de argumentos" "error"
 
 # STRESS TEST: PIPELINE LARGO (200 PROCESOS)
-PIPE_CHAIN=$(printf 'grep . | %.0s' {1..120}; echo tail -n 1)
+PIPE_CHAIN=$(printf 'grep . | %.0s' {1..100}; echo tail -n 1)
 run_test "cat $TEST_FILE | $PIPE_CHAIN" "Pipeline de 200 procesos con grep"
 
 
